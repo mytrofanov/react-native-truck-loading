ace6af4538b57731e177f2e6ac71890c8c806f1f
mytrofanov <mytrofanovmaxym@gmail.com>
2022-05-31 11:13:55 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import {styles} from "./styles/appStyles";
+import { Button, TextInput} from "@react-native-material/core";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+        <Button title="НОВА ЗМІНА" style={styles.buttonNewShift}/>
+        <Text style={styles.textStyles}>Середня загрузка: </Text>
+        <Text style={styles.tonnStyles}>131</Text>
+
+        <TextInput
+            variant="outlined" label="ЭКГ №:"
+            style={styles.textInputStyles}
+            keyboardType='numeric'
+        />
+        <TextInput
+            variant="outlined" label="Вигрузка т.:"
+            style={styles.textInputStyles}
+            keyboardType='numeric'
+        />
+        <Button title="НОВА ХОДКА" style={styles.buttonNewLoad}/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
