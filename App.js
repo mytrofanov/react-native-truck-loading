fc68d6a25b72b8c02ca2c9455fa9280bd95c7333
mytrofanov <mytrofanovmaxym@gmail.com>
2022-06-01 12:29:22 +0300

    small refactor

--- App.js
+++ App.js
@@ -3,19 +3,15 @@ import {StatusBar} from 'expo-status-bar';
 import {View} from 'react-native';
 import {styles} from "./src/styles/appStyles";
 import {Provider} from 'react-redux';
-import {store} from './src/Store/store'
+import store from './src/Store/store'
 import InputForm from "./src/Components/inputForm";
 import OutputData from "./src/Components/outputData";
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
 
-let persistor = persistStore(store);
 
 export default function App() {
 
     return (
         <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
 
                 <View style={styles.container}>
                 <OutputData/>
@@ -23,7 +19,6 @@ export default function App() {
                 <StatusBar style="auto"/>
             </View>
 
-            </PersistGate>
         </Provider>
     );
 }
--- src/Store/store.js
+++ src/Store/store.js
@@ -1,42 +1,8 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import truckReducer from '../../redux/truckSlice'
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist'
-// import storage from 'redux-persist/lib/storage';
-import hardSet from "redux-persist/es/stateReconciler/hardSet";
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-import AsyncStorage from '@react-native-community/async-storage';
-// import { AsyncStorage } from 'react-native';
 
-
-const reducers = combineReducers({
-    truckReducer: truckReducer,
+export default configureStore({
+    reducer: {
+        truckStore: truckReducer
+    },
 });
-
-const persistConfig = {
-    key: 'root',
-    storage: AsyncStorage,
-    // storage,
-    stateReconciler: hardSet,
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
-
-export const store =  configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
-});
-
